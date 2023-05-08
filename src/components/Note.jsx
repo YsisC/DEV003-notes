55bc6e20209bbb3005946d3d680890d5c094f56c
YsisC <ysisclongart@gmail.com>
2023-05-08 14:18:14 -0400

    integration note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,34 +1,25 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
 // import MdDelete from 'react-icons/md'
-import {MdDelete} from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
-function Note({title, content, displayName}) {
+function Note({ title, content, displayName }) {
   return (
-    // //example with props
-    // <div className={styles.note}>
-    //     <h1> Mi tituulo</h1>
-    //     <p>Esta es mi primera nota</p>
-
-    //     <button>
-    //           <MdDelete size={30}/> 
-    //           <></>
-    //      </button>
-    // </div>
-
-    //exaple with the template
+  
     <div className={styles.note}>
+      <div className={styles.note_main}>
         <h1> {title}</h1>
         <p>{content}</p>
-        <div className={styles.note_footer}>
-          <small>{displayName} </small>
-          {/* <small>20/20/17 </small> */}
+      </div>
+      <div className={styles.note_footer}>
+        <small>{displayName} </small>
+        {/* <small>20/20/17 </small> */}
         <button>
-              <MdDelete size={30}/> 
-           
-       </button>
-        </div>
-      
+          <MdDelete size={30} />
+
+        </button>
+      </div>
+
 
     </div>
   )
