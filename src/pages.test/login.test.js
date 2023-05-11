a98d4b96a0d6c987e030a3eb1d9cbf08a18d7395
YsisC <ysisclongart@gmail.com>
2023-05-11 11:03:13 -0400

    install jest

---
+++ src/pages.test/login.test.js
@@ -0,0 +1,34 @@
+// import { render, screen, fireEvent } from '@testing-library/react';
+
+// import Login from "../pages/login";
+
+// // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
+// // The mock has been moved to `__mocks__` folder to avoid duplication
+
+// test('Login page we create a entry', () => {
+  
+//     it('should display the button ', async() => {
+//       render(<Login />);
+//       const iptext = await screen.q
+//       const link = screen.getAllByRole('link', {
+//         name: / -/,
+//       });
+
+//       expect(link).toHaveLength(1);
+//     });
+
+// });
+
+// describe('Blog page', () => {
+//   describe('Render method', () => {
+//     it('should display the last 10 posts', () => {
+//       render(<Login />);
+
+//       const link = screen.getAllByRole('link', {
+//         name: /Blog -/,
+//       });
+
+//       expect(link).toHaveLength(1);
+//     });
+//   });
+// });
